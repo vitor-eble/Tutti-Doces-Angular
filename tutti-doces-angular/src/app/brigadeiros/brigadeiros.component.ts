723fa8c8e701026549f0e757ea60383b016b0659
Vitor Eble <vitoreble06@gmail.com>
2025-01-28 18:10:34 -0300

    .

--- tutti-doces-angular/src/app/brigadeiros/brigadeiros.component.ts
+++ tutti-doces-angular/src/app/brigadeiros/brigadeiros.component.ts
@@ -3,10 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-brigadeiros',
   standalone: false,
-  
+
   templateUrl: './brigadeiros.component.html',
   styleUrl: './brigadeiros.component.css'
 })
 export class BrigadeirosComponent {
 
+  brigadeiro: string = 'https://dolcebrigadeiro.ch/modules/advancedcms/uploads/AdvancedCmsBlockImage/28-Brigadeiro_Klassisch.png';
+
+  brigadeiroDeCoco: string = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTH4HNXeE8qlw-lgVSTx2pF6-HMDUgNrI1efw&s';
+
+  brigadeiroNinho: string = 'https://dolcebrigadeiro.ch/modules/advancedcms/uploads/AdvancedCmsBlockImage/31-Brigadeiro_Beijinho.png';
 }
