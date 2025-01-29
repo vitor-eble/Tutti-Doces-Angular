f1e5b27da4604279da655999571e2ba4edc85780
Vitor Eble <vitoreble06@gmail.com>
2025-01-28 21:47:23 -0300

    .

--- tutti-doces-angular/src/app/brigadeiros/brigadeiros.component.ts
+++ tutti-doces-angular/src/app/brigadeiros/brigadeiros.component.ts
@@ -10,8 +10,12 @@ import { Component } from '@angular/core';
 export class BrigadeirosComponent {
 
   brigadeiro: string = 'https://dolcebrigadeiro.ch/modules/advancedcms/uploads/AdvancedCmsBlockImage/28-Brigadeiro_Klassisch.png';
+  chocolateAoLeite: string = 'https://dolcebrigadeiro.ch/modules/advancedcms/uploads/AdvancedCmsBlockImage/48-Brigadeiro_Passionsfrucht_Schokolade.png';
+  casadinho: string = 'https://dolcebrigadeiro.ch/modules/advancedcms/uploads/AdvancedCmsBlockImage/29-Brigadeiro_Casadinho.png';
+  beijinho: string = 'https://dolcebrigadeiro.ch/modules/advancedcms/uploads/AdvancedCmsBlockImage/31-Brigadeiro_Beijinho.png';
+  chocolateBranco: string = 'https://dolcebrigadeiro.ch/modules/advancedcms/uploads/AdvancedCmsBlockImage/45-Brigadeiro_Zitrone.png';
+  amendoin: string = 'https://dolcebrigadeiro.ch/modules/advancedcms/uploads/AdvancedCmsBlockImage/38-Brigadeiro_Erdnuss.png';
+  amendoa: string = 'https://dolcebrigadeiro.ch/modules/advancedcms/uploads/AdvancedCmsBlockImage/37-Brigadeiro_Mandel.png';
+  marshmallow: string = 'https://dolcebrigadeiro.ch/modules/advancedcms/uploads/AdvancedCmsBlockImage/30-Brigadeiro_Marshmellow.png'
 
-  brigadeiroDeCoco: string = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTH4HNXeE8qlw-lgVSTx2pF6-HMDUgNrI1efw&s';
-
-  brigadeiroNinho: string = 'https://dolcebrigadeiro.ch/modules/advancedcms/uploads/AdvancedCmsBlockImage/31-Brigadeiro_Beijinho.png';
 }
