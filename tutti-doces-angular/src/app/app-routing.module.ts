45daecf60cbaacfee822644861a7f6ada73fe635
Vitor Eble <vitoreble06@gmail.com>
2025-01-30 21:09:59 -0300

    .

--- tutti-doces-angular/src/app/app-routing.module.ts
+++ tutti-doces-angular/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BrigadeirosComponent } from './brigadeiros/brigadeiros.component';
+import { InicioComponent } from './inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'brigadeiros', component: BrigadeirosComponent },
+  {path: 'inicio', component: InicioComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- tutti-doces-angular/src/app/inicio/inicio.component.ts
+++ tutti-doces-angular/src/app/inicio/inicio.component.ts
@@ -3,10 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-inicio',
   standalone: false,
-  
+
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
 export class InicioComponent {
 
+  verMais: string = 'https://cdn-icons-png.flaticon.com/128/11431/11431124.png'
+
+  brigadeiro: string = 'https://dolcebrigadeiro.ch/modules/advancedcms/uploads/AdvancedCmsBlockImage/28-Brigadeiro_Klassisch.png';
+  chocolateAoLeite: string = 'https://dolcebrigadeiro.ch/modules/advancedcms/uploads/AdvancedCmsBlockImage/48-Brigadeiro_Passionsfrucht_Schokolade.png';
+  casadinho: string = 'https://dolcebrigadeiro.ch/modules/advancedcms/uploads/AdvancedCmsBlockImage/29-Brigadeiro_Casadinho.png';
+  beijinho: string = 'https://dolcebrigadeiro.ch/modules/advancedcms/uploads/AdvancedCmsBlockImage/31-Brigadeiro_Beijinho.png';
 }
